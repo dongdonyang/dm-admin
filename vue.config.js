3a7ad0c81b7723d47e84f331cfb531b09d5691eb
dongdonyang <37789273+dongdonyang@users.noreply.github.com>
2019-09-29 17:40:21 +0800

    登录

--- vue.config.js
+++ vue.config.js
@@ -30,5 +30,20 @@ module.exports = {
       axios: "axios",
       element: "element"
     }
+  },
+
+//  todo px转rem配置
+  css: {
+    loaderOptions: {
+      postcss: {
+        plugins: [
+          require('postcss-pxtorem')({
+            rootValue : 16, // 换算的基数
+            selectorBlackList  : [], // 忽略转换正则匹配项
+            propList   : ['*'],
+          }),
+        ]
+      }
+    }
   }
 };
